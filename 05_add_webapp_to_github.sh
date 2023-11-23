d8b47baeebb2e0666cd7d0013fda37f0f9b83b5b
kiws <62125060+iwatadive28@users.noreply.github.com>
2023-11-23 12:00:15 +0900

    Update 05_add_webapp_to_github.sh

--- 05_add_webapp_to_github.sh
+++ 05_add_webapp_to_github.sh
@@ -10,5 +10,8 @@ git add *
 git status
 git commit -m "first commit"
 
-git remote add origin https://github.com/ChallengeClub/isucon12q3.git
+# アカウント名, アクセストークン、URLは書き換えてください
+# git remote add origin https://{Githubアカウント名}:{アクセストークン}@github.com/kiws-isucon-bot/isucon13_f1.git
+# git remote add origin https://{Githubアカウント名}:{アクセストークン}@github.com/kiws-isucon-bot/isucon13_f2.git
+# git remote add origin https://github.com/ChallengeClub/isucon12q3.git
 git push -u origin main
