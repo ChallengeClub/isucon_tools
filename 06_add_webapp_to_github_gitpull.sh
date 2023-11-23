b0a52cd082dcd3d430e730c0a898199398bf90a3
kiws <62125060+iwatadive28@users.noreply.github.com>
2023-11-23 14:11:23 +0900

    git pullがうまくいかないときの対策を追加

--- 06_add_webapp_to_github_gitpull.sh
+++ 06_add_webapp_to_github_gitpull.sh
@@ -9,16 +9,25 @@ git config --global credential.helper 'cache --timeout=2592000'
 
 cd ~/webapp
 git init
+# 空コミットをしておく
+git commit --allow-empty -m "initial commit"
 
 # アカウント名, アクセストークン、URLは書き換えてください
 # git remote add origin https://kiws-isucon-bot:{アクセストークン}@github.com/kiws-isucon-bot/isucon13_f1.git
 # git remote add origin https://kiws-isucon-bot:{アクセストークン}@github.com/kiws-isucon-bot/isucon13_f2.git
 
+# git pull がうまくいかないときは、以下で強制的に同期させる
+git reset --hard origin/main
 git pull origin main
 
-# webapp を追加してpushします
+# webapp を追加してpushする
 git add *
 git status
 git commit -m "add webapp first"
 
 git push -u origin main
+
+# pushしたコミットをなかったことにしたいとき。
+# git revert HEAD
+# git push origin main
+
